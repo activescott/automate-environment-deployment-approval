1439caebbd60bf4da88070a840e2e0e5de99a488
scott willeke <scott@willeke.com>
2023-05-02 11:03:40 -0700

    fix: sometimes awaiting workflows were not found (#31)

--- src/approver.ts
+++ src/approver.ts
@@ -1,5 +1,6 @@
-import * as core from "@actions/core"
 import { inspect } from "node:util"
+import * as trace from "./trace"
+import { setFailed } from "@actions/core"
 import { Octo, PartialWorkflowRun, WorkflowRun } from "./octo"
 
 export async function findAndApproveDeployments(
@@ -8,7 +9,13 @@ export async function findAndApproveDeployments(
   actorAllowList: string[],
   environmentAllowList: string[]
 ): Promise<void> {
+  trace.debug("Fetching runs for repo:", repo)
   const waitingRunsResponse = await octo.getWaitingWorkflowRuns(repo)
+  trace.debug(
+    "Found %s waiting runs as follows: ",
+    waitingRunsResponse.data.total_count,
+    waitingRunsResponse.data.workflow_runs.map((run) => run.display_title)
+  )
   const runs: PartialWorkflowRun[] = validateRuns(
     waitingRunsResponse.data.workflow_runs
   )
@@ -19,10 +26,10 @@ export async function findAndApproveDeployments(
     repo,
     environmentAllowList
   )
-  core.notice(
-    `Found ${deploysToApprove.length} deploys that should be approved...`
+  trace.notice(
+    "Found %d deploys that should be approved...",
+    deploysToApprove.length
   )
-
   for await (const deploy of deploysToApprove) {
     if (!deploy) {
       // this case is actually prevented with prior filtering, but tsc doesn't seem to get that :/
@@ -35,12 +42,18 @@ export async function findAndApproveDeployments(
     const actor = run.actor
     const environment = deploy.environment
     try {
-      core.info(
-        `Approving deployment to ${environment.name} triggered by ${actor.login} for run ${run.display_title}...`
+      trace.info(
+        "Approving deployment to %s triggered by %s for run %s...",
+        environment.name,
+        actor.login,
+        run.display_title
       )
       await octo.approveDeployment(repo, run, deploy.environment)
-      core.notice(
-        `Approved deployment to ${environment.name} triggered by ${actor.login} for run ${run.display_title}.`
+      trace.notice(
+        "Approved deployment to %s triggered by %s for run %s.",
+        environment.name,
+        actor.login,
+        run.display_title
       )
     } catch (err) {
       const msg = `Failed to approve deployment for run '${
@@ -48,7 +61,7 @@ export async function findAndApproveDeployments(
       }' (${run.id}) to environment '${
         deploy.environment.name
       }'. The current user is '${await octo.currentUser()}' and the error was: ${err}`
-      core.setFailed(msg)
+      setFailed(msg)
     }
   }
 }
@@ -82,39 +95,49 @@ async function filterDeploymentsToApprove(
   repo: { owner: string; repo: string },
   environmentAllowList: string[]
 ): Promise<DeployForApproval[]> {
+  trace.debug(
+    "Filtering the following workflow runs:",
+    runs.map((run) => `#${run.id}: ${run.display_title}}`)
+  )
   const filteredDeployPromises = runs.map(async (run) => {
     const actor = run.actor && run.actor.login
     if (!actorAllowList.includes(actor)) {
-      core.warning(
-        `Run '${run.display_title}' (${
-          run.id
-        }) has a deployment pending approval but the actor '${actor}' is not allowed. Allowed actors are '${inspect(
-          actorAllowList
-        )}' and are specified in the \`actor_allow_list\` input.`
+      trace.warning(
+        "Run '%s (%s)' has a deployment pending approval but the actor '%s' is not allowed. Allowed actors are '%O' and are specified in the `actor_allow_list` input.",
+        run.display_title,
+        run.id,
+        actor,
+        actorAllowList
       )
       return null
     }
     const deploys = await octo.getPendingDeploymentsForRun(repo, run.id)
+    trace.debug(
+      "Found the following deploys for workflow run: " +
+        inspect(deploys.data.map((deploy) => deploy.environment.name))
+    )
     const currentUser = await octo.currentUser()
     const approvable = deploys.data.filter((deploy) => {
       if (!deploy.environment.name) {
         throw new Error("expected environment to have name")
       }
       if (!environmentAllowList.includes(deploy.environment.name)) {
-        core.warning(
-          `Run '${run.display_title}' (${
-            run.id
-          }) has a deployment pending approval but it is not to an allowed environment: '${
-            deploy.environment.name
-          }'. Allowed environments are ${inspect(
-            environmentAllowList
-          )} and are specified in the \`environment_allow_list\` input.`
+        trace.warning(
+          "Run '%s' (%s) has a deployment pending approval but it is not to an allowed environment: '%s'. Allowed environments are '%O' and are specified in the `environment_allow_list` input.",
+          run.display_title,
+          run.id,
+          deploy.environment.name,
+          environmentAllowList
         )
         return false
       }
       if (!deploy.current_user_can_approve) {
-        core.warning(
-          `The current user (${currentUser.login}) cannot approve deployment for run '${run.display_title}' (${run.id}) to environment '${deploy.environment.name}'. The github_token input determines the current user and it must be from a 'required reviewer' and must have the 'repo' scope. See https://github.com/activescott/automate-environment-deployment-approval#token-permissions-permissions for more information.`
+        trace.warning(
+          "The current user (%s) cannot approve deployment for run '%s' (%s) to environment '%s'. The github_token input determines the current user and it must be from a 'required reviewer' and must have the 'repo' scope. See https://github.com/activescott/automate-environment-deployment-approval#token-permissions-permissions for more information.",
+          currentUser.login,
+          run.display_title,
+          run.id,
+          deploy.environment.name
         )
         // This WILL fail later if we allow it to continue and doesn't have any better error information (only 'HttpError: Validation Failed')
         return false
@@ -123,8 +146,11 @@ async function filterDeploymentsToApprove(
     })
     if (approvable.length > 0) {
       const deploy = approvable[0]
-      core.info(
-        `Deployment '${run.display_title}' (${run.id}) to environment '${deploy.environment.name}' will be approved...`
+      trace.info(
+        `Deployment '%s' (%s) to environment '%s' will be approved...`,
+        run.display_title,
+        run.id,
+        deploy.environment.name
       )
       if (!deploy.environment.name) {
         throw new Error("environment does not have name")
---
+++ src/debug.test.ts
@@ -0,0 +1,25 @@
+import { findAndApproveDeployments } from "./approver"
+import { createOcto, Octo } from "./octo"
+import * as github from "@actions/github"
+
+/**
+ * This is used to test live scenarios against activescott/serverless-aws-static-file-handler
+ * Run it like this:
+ * GITHUB_TOKEN=ghp_... npm run test -- debug
+ */
+test.skip("debug serverless-aws-static-file-handler", async () => {
+  if (process.env.GITHUB_TOKEN === undefined) {
+    throw new Error(
+      "A GITHUB_TOKEN environment variable must be defined with a valid github token"
+    )
+  }
+  const repo = {
+    owner: "activescott",
+    repo: "serverless-aws-static-file-handler",
+  }
+  const octo: Octo = createOcto(
+    repo,
+    github.getOctokit(process.env.GITHUB_TOKEN)
+  )
+  await findAndApproveDeployments(octo, repo, ["DONT APPROVE"], ["aws"])
+})
